12367b4e26bfb8350f309c1e3255bbb0ff8279c5
Don Henton <dhenton@amfam.com>
2018-05-25 12:09:38 -0500

    initial commit of gh-pages

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  securityAPIURL: 'https://donhenton-spring-boot.herokuapp.com/sec/',
-  birtAPIURL: 'https://donhenton-spring-boot.herokuapp.com/birt/',
-};
