aa49a1ff84bac3d60c8724605484b5a1e557e3e1
Don Henton <donhenton@gmail.com>
2018-06-28 08:38:36 -0500

    cleanup

--- src/app/pages/child-components/child-page/child-page.component.ts
+++ src/app/pages/child-components/child-page/child-page.component.ts
@@ -5,12 +5,12 @@ import { Component, OnInit } from '@angular/core';
    template: `
    <h3>Child Components</h3>
   <div class="row">The content below is inserted via child routes</div>
-  
-  
+
+
   <div class="row child-area"><router-outlet></router-outlet></div>
-  
-  
-  
+
+
+
   `,
   styleUrls: ['./child-page.component.scss']
 })
