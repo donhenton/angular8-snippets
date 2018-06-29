cff50aee17886adcb24450019f4b5454e133c853
Don Henton <donhenton@gmail.com>
2018-06-29 08:10:23 -0500

    text change

--- src/app/pages/child-components/child-page/child-page.component.ts
+++ src/app/pages/child-components/child-page/child-page.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
    template: `
    <h3>Child Components</h3>
   <div class="row">The content below is inserted via child routes.</div><div class="row"> There is a separate module that contains
-  the child routes, so that they can be defined elsewhere that the main app.module</div>
+  the child routes, so that they can be defined someplace other than the main app.module</div>
 
   <div class="row child-area"><router-outlet></router-outlet></div>
 
